53ba8519314475be9ffa8e54e3be1ee74ef902c1
HaBellHyeon <jhjm5658@naver.com>
2024-02-01 22:26:17 +0900

    등록api

---
+++ src/main/java/com/hajong/springspringweb/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.hajong.springspringweb.service.posts;
+
+import com.hajong.springspringweb.domain.posts.PostsRepository;
+import com.hajong.springspringweb.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+}
