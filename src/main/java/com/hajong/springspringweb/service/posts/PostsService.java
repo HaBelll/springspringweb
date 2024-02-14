0e30861d02a0256db5c2d9262270aa03ccfc8e03
HaBellHyeon <jhjm5658@naver.com>
2024-02-14 22:20:32 +0900

    게시글 수정 만들기

--- src/main/java/com/hajong/springspringweb/service/posts/PostsService.java
+++ src/main/java/com/hajong/springspringweb/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.hajong.springspringweb.service.posts;
 
 import com.hajong.springspringweb.domain.posts.Posts;
 import com.hajong.springspringweb.domain.posts.PostsRepository;
+import com.hajong.springspringweb.web.dto.PostsListResponseDto;
 import com.hajong.springspringweb.web.dto.PostsResponseDto;
 import com.hajong.springspringweb.web.dto.PostsSaveRequestDto;
 import com.hajong.springspringweb.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,6 +38,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new
+                        )
     }
 
 }
